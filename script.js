bb8f33ad1aeca39a2e2a303e1571b9128424941b
Taanveer22 <taanveer469@gmail.com>
2025-07-16 02:23:44 +0600

    alhamdulliah spinner added to search box

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('connected');
+console.log("connected");
+
+const loadAllPhones = () => {
+  console.log("3 seonds later ....");
+  document.getElementById("spinner").style.display = "none";
+};
+
+const handleSearch = () => {
+  console.log(" search btn clicked");
+  document.getElementById("spinner").style.display = "block";
+  setTimeout(() => {
+    loadAllPhones();
+  }, 3000);
+};
