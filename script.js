2a01fe4b587f8b17909f11a92a0242a13c9b89fa
Taanveer22 <taanveer469@gmail.com>
2025-07-17 12:51:57 +0600

    alhamdulliah show card data in the ui

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 console.log("connected");
 
-const loadAllPhones = async (status) => {
+const loadAllPhones = async (status, searchText) => {
+  console.log(searchText);
   //   console.log("3 seonds later ....");
   //   console.log(status);
   document.getElementById("spinner").style.display = "none";
 
   const res = await fetch(
-    "https://openapi.programming-hero.com/api/phones?search=iphone"
+    `https://openapi.programming-hero.com/api/phones?search=${
+      searchText ? searchText : "iphone"
+    }`
   );
   //   console.log(res);
   const data = await res.json();
-  //console.log(data.data);
+  // console.log(data.data);
 
   if (status === true) {
     displayAllPhones(data.data);
@@ -20,15 +23,52 @@ const loadAllPhones = async (status) => {
 };
 
 const displayAllPhones = (phones) => {
-  console.log(phones);
+  // console.log(phones);
+  const phonesContainer = document.getElementById("phones-container");
+  phones.forEach((item) => {
+    // console.log(item);
+    const { brand, phone_name, slug, image } = item;
+    // console.log(brand, phone_name, slug, image);
+    const div = document.createElement("div");
+    div.innerHTML = `
+        <div class="card bg-gray-400 m-2">
+            <figure class="px-10 pt-10">
+                    <img src="${image}"class="rounded-xl"/>
+            </figure>
+            <div class="card-body items-center text-center">
+                <h2 class="card-title">${brand}</h2>
+                <p>${phone_name}</p>
+                <div class="card-actions">
+                     <button onclick="handleShowDetails('${slug}')"
+                              class="btn btn-primary">
+                              Show Details
+                     </button>
+                </div>
+            </div>
+        </div>
+    `;
+    phonesContainer.append(div);
+  });
+};
+
+// onclick handler for show details button
+const handleShowDetails = async (slug) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/phone/${slug}`
+  );
+  // console.log(res);
+  const data = await res.json();
+  console.log(data.data);
 };
 
 // onclick handler for search button
 const handleSearch = () => {
-  //   console.log(" search btn clicked");
+  console.log("search btn clicked");
   document.getElementById("spinner").style.display = "block";
+  const inputSearch = document.getElementById("search-box").value;
+  // console.log(inputSearch);
   setTimeout(() => {
-    loadAllPhones();
+    loadAllPhones(false, inputSearch);
   }, 3000);
 };
 
@@ -39,4 +79,4 @@ const handleShowAll = () => {
 };
 
 // final function invocation
-loadAllPhones();
+loadAllPhones(false, "iphone");
